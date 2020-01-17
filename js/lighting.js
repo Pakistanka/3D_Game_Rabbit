7e4c5cc0b23c1ca1e313f1e5b689474609b6512b
Anastasia Polozova <polozova.anastasi@gmail.com>
2020-01-17 12:33:40 +0200

    create heros, monster and movements

--- js/lighting.js
+++ js/lighting.js
@@ -1,20 +1,18 @@
 function createLights() {
     globalLight = new THREE.AmbientLight(0xffffff, .9);
-
-    // setup the position of the source of light
-    shadowLight = new THREE.DirectionlLight(0xffffff, 1);
+  
+    shadowLight = new THREE.DirectionalLight(0xffffff, 1);
     shadowLight.position.set(-30, 40, 20);
     shadowLight.castShadow = true;
     shadowLight.shadow.camera.left = -400;
     shadowLight.shadow.camera.right = 400;
     shadowLight.shadow.camera.top = 400;
-    shadowLight.shadow.camera.down = -400;
+    shadowLight.shadow.camera.bottom = -400;
     shadowLight.shadow.camera.near = 1;
     shadowLight.shadow.camera.far = 2000;
-    shadowLight.shadow.mapSize.width = shadowLight.shadow.mapSize.height = 2000;
-
+    shadowLight.shadow.mapSize.width = shadowLight.shadow.mapSize.height = 2048;
+  
     scene.add(globalLight);
     scene.add(shadowLight);
-}
-
-// light.castShadow = true;
+    
+  }
